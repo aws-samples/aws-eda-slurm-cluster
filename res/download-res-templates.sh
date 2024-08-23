f59a3be11ebaa56e69d97dd1329278e9dedfe10a
Allan Carter <cartalla@amazon.com>
2024-08-23 15:27:06 +0000

    Add res-demo template that allows CIDR and LDIF configuration
    The LDIF file creates admins, users, and groups in the regular USERS OU.
    It also creates a RES OU with admins, users, and groups in it.
    The RES OU users and groups are prefixed with "res".
    
    Resolves #251
    
    Add install_vscode role and playbook
    
    Update custom-amis.md

---
+++ res/download-res-templates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Download the original templates from s3
+# This is so that they can be used to create modified versions
+
+script_dir=$(dirname $(realpath $0))
+cd $script_dir
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res-demo-stack.yaml res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/bi.yaml res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/net/hpc_large_scale/assets/main.yaml res-demo-original/networking.yaml
+
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res.ldif res-demo-original/.
