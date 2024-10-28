b9eb695294ba57a6d2c45fb1ce88bb6d3f1204fd
Allan Carter <cartalla@amazon.com>
2024-10-28 16:03:45 -0500

    Update RES templates (#276)
    Get latest RES templates
    
    Create separate BI templates and RES-only templates.
    
    Change default keycloak instance type from t3.micro to c7a.medium.
    
    Resolves #275

--- res/download-res-templates.sh
+++ res/download-res-templates.sh
@@ -7,8 +7,10 @@
 
 script_dir=$(dirname $(realpath $0))
 cd $script_dir
-aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res-demo-stack.yaml res-demo-original/.
-aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/bi.yaml res-demo-original/.
-aws s3 cp s3://aws-hpc-recipes/main/recipes/net/hpc_large_scale/assets/main.yaml res-demo-original/networking.yaml
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/bi.yaml               res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/keycloak.yaml         res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res-demo-stack.yaml   res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res-sso-keycloak.yaml res-demo-original/.
+aws s3 cp s3://aws-hpc-recipes/main/recipes/net/hpc_large_scale/assets/main.yaml          res-demo-original/networking.yaml
 
 aws s3 cp s3://aws-hpc-recipes/main/recipes/res/res_demo_env/assets/res.ldif res-demo-original/.
